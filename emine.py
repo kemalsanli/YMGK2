f51040aa15e04b67befd5fc9c8f119711a00117c
FatihUludağ <fatih.dag66@hotmail.com>
2020-12-26 17:29:14 +0300

    ip-3

--- emine.py
+++ emine.py
@@ -1,38 +1,17 @@
-import random as rnd
-# import the library module 
-import sys 
-import hashlib 
-def randomMillion_Bit(wp2Out):
-    xExVal=wp2Out
-    randomSize=100
-    rand_Bit=list()
-    for i in range(randomSize):
-        xNewVal = xExVal*(1 - xExVal)*4
-        if(xNewVal < 0.5):   
-            rand_Bit.insert(i, 0)
-        else:
-            rand_Bit.insert(i, 1)
-        xExVal = xNewVal
-    return rand_Bit
- 
-work_P2=list()
-for i in range(12):
-    rnd_Number=rnd.random()
-    work_P2.append(rnd_Number)
-    print(rnd_Number)
-    
-def Work_Package(workP2Out,height,width,isColor):
-    if(isColor==True):
-        size = 3*height*width*8  #ip5 paketi resim boyutu
-    if(isColor==False):
-        size = height*width*8
-    print('toplam çıktı:'+ str(size))
 
+import random as rnd 
+
+def Work_Package(workP2Out,isColor,uzunluk):
+    if(isColor==True):
+        size = 8*3*uzunluk  #ip5 paketi resim boyutu
+    if(isColor==False):
+        size = 3*8*uzunluk
+    print('toplam çıktı:'+ str(size))
     tempWork3=[]
     Work3=[]
     while(True):
         if(size > len(tempWork3)):
-          tempWork3.extend((randomMillion_Bit(work_P2[int(rnd.random())])))
+          tempWork3.extend((randomMillion_Bit(workP2Out[int(rnd.random())])))
         else:
             break
         
@@ -42,22 +21,47 @@ def Work_Package(workP2Out,height,width,isColor):
     print('son liste:' + str(len(Work3)))
     return Work3
    
-gelendeger=[0,1,1,0,1,1,0,0,0,1,1,0,0,0,1,0,1,0,1,1,0,0,0,0]
+    
+
+def randomMillion_Bit(wp2Out):
+    xExVal=wp2Out
+    randomSize=100
+    rand_Bit=list()
+    for i in range(randomSize):
+        xNewVal = xExVal*(1 - xExVal)*4
+        if(xNewVal < 0.5):   
+            rand_Bit.insert(i, rnd.randint(0,128))
+        else:
+            rand_Bit.insert(i, rnd.randint(128,255))
+        xExVal = xNewVal
+    return rand_Bit
+
 def xor(Work3, gelendeger, n): 
     #print("gelendeger :",gelendeger)
     #print("work 3 :",Work3)
-    ans = ""
+    ans = []
     for i in range(n): 
        
-        if (Work3[i] == gelendeger[i]):  
-            ans += "0"
+        ans.append(Work3[i] ^ gelendeger[i])
             
-        else:
-            ans += "1"
             
     return ans 
+# Create your views here.
 
-#(Work_Package(workP2Out, 1, 1,True))
-print("-------------------------------XOR----------------------------------------")
-print(xor(Work_Package(work_P2, 1, 1,True), gelendeger,24 ))
-            
+    
+gelendeger=[107,116,61,144,204,8,62,225,191,177,84,158,51,46,207,216,15,231,107,69,37,37,198,18,246,254,37,234,71,77,245,134]
+print(gelendeger,"gelendeger")
+i=0
+workP2Out=list()
+    
+while(i<12):
+    rnd_Number=rnd.random()
+    workP2Out.append(rnd_Number)
+    print(rnd_Number)
+    i=i+1
+uzunluk=len(gelendeger)
+y=xor(Work_Package(workP2Out,True,uzunluk), gelendeger,len(gelendeger))
+print("xorlanmış",y)
+
+       
+        
