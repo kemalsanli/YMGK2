0f26430be5c5feab2cbd348ddf7353cbfaa4196d
Kemal SANLI <kemalsanli@outlook.com.tr>
2021-01-07 13:33:32 +0300

    Uygulama ikonu eklendi.

--- arayüz.py
+++ arayüz.py
@@ -17,6 +17,7 @@ root.title('YMGK2 XOR')
 #root.geometry("200x100")
 root.eval('tk::PlaceWindow . center')
 #root.resizable(False, False)
+root.iconphoto(False, tk.PhotoImage(file='icon.png'))
 
 
 gelendeger=[207,192,0,74,230,6,155,195,0,118,210,155,250,83,184,194,112,220,68,126,12,7,185,255,89,234,249,47,216,148,125,165,111,120,199,249,219,61,78,88,18,11,169,206,23,125,40,42,75,43,183,72,144,38,77,183,162,189,171,214,208,144,248,169]
@@ -42,7 +43,7 @@ print("                                                                         
 def select_image():
     # grab a reference to the image panels
     global panelA, panelB
- 
+
     # open a file chooser dialog and allow the user to select an input
     # image
     path = filedialog.askopenfilename(initialdir = "/",title = "Resim Seçin",filetypes = (("Resim Dosyaları",".png .jpg .jpeg"),("Tüm Dosyalar",".*")))
@@ -54,13 +55,13 @@ def select_image():
 
         if  os.path.exists('key') == False:
             os.mkdir('key')
-            
+
         if  os.path.exists('temp') == False:
                 os.mkdir('temp')
         if  os.path.exists(('key/{}.png'.format(hashFile))):
             key = cv2.imread(('key/{}.png'.format(hashFile)))
             sifresiz = fatih.xor(gorsel,key)
-            
+
             kaydet.kaydet(sifresiz,'temp/sonuc.png')
             os.remove(('key/{}.png'.format(hashFile)))
         else:
@@ -68,11 +69,11 @@ def select_image():
 
             anahtar = fatih.anahtarOlustur(gorsel, keySource)
             sifrelenmis = fatih.xor(gorsel, anahtar)
-            
+
             kaydet.kaydet(sifrelenmis,'temp/sonuc.png')
             sifreliHash = hash.hashIt('temp/sonuc.png')
             kaydet.kaydet(anahtar,('key/{}.png'.format(sifreliHash)))
-        
+
         # load the image from disk, convert it to grayscale, and detect
         # edges in it
         dosyaac.Clear_Console()
@@ -80,20 +81,20 @@ def select_image():
         print("                                                                           Was")
         print("                                                                          Here")
         edged = cv2.imread('temp/sonuc.png')
-        
- 
+
+
         # OpenCV represents images in BGR order; however PIL represents
         # images in RGB order, so we need to swap the channels
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         edged = cv2.cvtColor(edged, cv2.COLOR_BGR2RGB)
-        
-        
-        
- 
+
+
+
+
         # convert the images to PIL format...
         image = Image.fromarray(image)
         edged = Image.fromarray(edged)
- 
+
         # ...and then to ImageTk format
         image = ImageTk.PhotoImage(image)
         edged = ImageTk.PhotoImage(edged)
@@ -103,12 +104,12 @@ def select_image():
             panelA = Label(image=image)
             panelA.image = image
             panelA.pack(side="left", padx=10, pady=10)
- 
+
             # while the second panel will store the edge map
             panelB = Label(image=edged)
             panelB.image = edged
             panelB.pack(side="right", padx=10, pady=10)
- 
+
         # otherwise, update the image panels
         else:
             # update the pannels
@@ -116,9 +117,9 @@ def select_image():
             panelB.configure(image=edged)
             panelA.image = image
             panelB.image = edged
-    btn.config(text="Yeniden Seç")      
+    btn.config(text="Yeniden Seç")
 
-    
+
 # b1=tk.Button(root,text="Dosya Seç",font=40,command=browsefunc)
 # spaceLabel = tk.Label(root, text= "                     ")
 # label1 = tk.Label(root, text= "Lütfen bir resim dosyası seçin.")
@@ -127,7 +128,7 @@ def select_image():
 # b1.pack()
 panelA = None
 panelB = None
- 
+
 # create a button, then when pressed, will trigger a file chooser
 # dialog and allow the user to select an input image; then add the
 # button the GUI
